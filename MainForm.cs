0fcc74c530837ac834b27a4c466fab71b18aa91e
Haile-Ven <haileyesustilahun.smuc.edu.et@gmail.com>
2026-09-24 21:52:37 +0300

    msg

--- MainForm.cs
+++ MainForm.cs
@@ -617,7 +617,7 @@ namespace Common_Tasks
                             }
                         }
                     }
-                    toastNotification.Show($"{Name} cleared. Deleted profile named {subKeyName}.", "SUCCESS", true);
+                    toastNotification.Show($"Deleted profile named {subKeyName} successfully.", "SUCCESS", true);
                 }
                 else
                 {
